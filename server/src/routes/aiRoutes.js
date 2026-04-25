dfa1f1053e850eb208b7fa21786e831b455b636c
Mohamed Mourrane <mourranemohamed2020@gmail.com>
2026-04-25 10:00:53 +0100

    delete

--- server/src/routes/aiRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { askIA } = require("../controllers/aiController");
-const authenticate = require("../middleware/authMiddleware");
-const router = express.Router();
-
-router.post("/ask", authenticate, askIA);
-
-module.exports = router;
